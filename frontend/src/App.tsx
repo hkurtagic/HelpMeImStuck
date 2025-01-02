2d67ab5df0e65921d314ee013af28f87b1456c4d
Nikyar Karimi <nikyar.karimi@stud.fh-campuswien.ac.at>
2025-01-02 23:35:30 +0100

    Added card, input, label, and login-form via shadcn and modeled the login form in Login.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,17 @@
-import {useState} from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import {Button} from "@/components/ui/button.tsx";
 import React from "npm:@types/react@^18.3.12";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "@/pages/Login.tsx";
+import Dashboard from "@/pages/Dashboard.tsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count: number) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/:dashboard" element={<Dashboard />} />
+          </Routes>
+      </BrowserRouter>
   );
 }
 
