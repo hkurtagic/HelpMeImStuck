e537516631a426155ae0051d34d0fbb44e053c5b
Nini3010 <stefan-wesely@gmx.at>
2025-01-08 17:17:38 +0100

    user, department and role prefill

--- backend/model/dbtypes.ts
+++ backend/model/dbtypes.ts
@@ -1,26 +1,26 @@
 interface dbUser {
-    pk_user_id: string;
-    user_name: string;
-    password_hash: string;
-    created_at: number;
-    updated_at: number;
+  pk_user_id: string;
+  user_name: string;
+  password_hash: string;
+  created_at: number;
+  updated_at: number;
 }
 interface dbDepartments {
-    dept_id: number;
-    deparment_name: string;
-    description: string;
+  pk_department_id: number;
+  department_name: string;
+  description: string;
 }
 interface dbAction {
-    action_id: number;
-    action_name: string;
+  pk_action_id: number;
+  action_name: string;
 }
 interface dbRole {
-    role_id: number;
-    role_name: string;
-    description: string;
-    fk_department_id: number;
-    pk_action_id: number;
-    action_name: string;
+  pk_role_id: number;
+  role_name: string;
+  description: string;
+  fk_department_id: number;
+  pk_action_id: number;
+  action_name: string;
 }
 
-export type { dbUser, dbDepartments, dbAction, dbRole };
+export type { dbAction, dbDepartments, dbRole, dbUser };
