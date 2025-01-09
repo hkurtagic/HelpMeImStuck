38981587f8bf976e39df721e9791c6406bd3caaa
Nini3010 <stefan-wesely@gmx.at>
2025-01-09 17:48:29 +0100

    jank ticket ep

--- backend/route/Tickets.ts
+++ backend/route/Tickets.ts
@@ -1,19 +1,15 @@
 import { Hono } from "hono";
-import JWTAuthChecker from "../handler/AuthenticationHandler.ts";
-import { Ticket } from "../../shared_types/communication_types.ts";
-import { jwt_payload } from "../model/api_types.ts";
-import db from "../service/database.ts";
+import JWTAuthChecker from "@backend/handler/AuthenticationHandler.ts";
+import { Ticket } from "@shared/shared_types.ts";
+import {
+  dbAction,
+  dbDepartments,
+  dbRole,
+  dbUser,
+} from "@backend/model/dbtypes.ts";
+import db from "@backend/service/database.ts";
 
 const tickets = new Hono();
-const renameKeys = (keysMap, obj) =>
-  Object.keys(obj).reduce(
-    (acc, key) => ({
-      ...acc,
-      ...{ [keysMap[key] || key]: obj[key] },
-    }),
-    {},
-  );
-
 // get tickets of department
 tickets.get("/", JWTAuthChecker, async (c) => {
   const req = await c.req.json() as ({ department_id: number });
@@ -21,6 +17,24 @@ tickets.get("/", JWTAuthChecker, async (c) => {
   if (!(tickets instanceof Error)) {
     return c.json(tickets, 200);
   }
+});
+tickets.get("/create", JWTAuthChecker, async (c) => {
+  const req = await c.req.json() as Ticket;
+  const author_id = db.getUserByUsername(req.author);
+  if ((author_id == undefined)) {
+    return c.json({ message: "User does not exist" }, 400);
+  }
+  if ((author_id instanceof Error)) {
+    console.log(author_id);
+    return c.json({ message: "Servererror with provided Data" }, 500);
+  }
+  const tickets = db.addTicket(
+    req.title,
+    req.description,
+    author_id.pk_user_id,
+    req.images,
+  );
+  return c.json(tickets, 200);
 });
 
 export default tickets;
