e5f7fffbce20cc8215d641b2c4374ada21f90585
Nini3010 <stefan-wesely@gmx.at>
2025-01-09 21:15:26 +0100

    updated jwt to account for deleted users; changed some type layout

--- backend/model/api_types.ts
+++ backend/model/api_types.ts
@@ -1,11 +1,7 @@
-interface LoginRequestBody {
-    username: string;
-    password: string;
-}
-type jwt_payload = {
-    user_id: string;
-    iat: number;
-    exp: number;
+type jwt_custom_payload = {
+  user_id: string;
+  iat: number;
+  exp: number;
 };
 
-export type { LoginRequestBody, jwt_payload };
+export type { jwt_custom_payload };
