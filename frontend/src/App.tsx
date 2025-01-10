25bb11db5a70d81b814aeb2cfc5242528c6d6e6c
Nini3010 <stefan-wesely@gmx.at>
2025-01-10 21:37:08 +0100

    ticket creation fronted done; applied usercontext over all routes, added handler for applying auth heander to requests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,27 @@
 import "@/App.css";
 import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "@/pages/LoginPage.tsx";
 import DashboardPage from "@/pages/DashboardPage.tsx";
 import ActivityPage from "@/pages/ActivityPage.tsx";
 import PasswordResetPage from "@/pages/PasswordResetPage.tsx";
 import StatisticsPage from "@/pages/StatisticsPage.tsx";
+import UserContextProvider from "./components/UserContextProvider";
 
 function App() {
-  return (
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<LoginPage />} />
-              <Route path="/:dashboard" element={<DashboardPage />} />
-              <Route path="/activities" element={<ActivityPage />} />
-              <Route path="/password-reset" element={<PasswordResetPage/>} />
-              <Route path="/statistics" element={<StatisticsPage/>} />
-          </Routes>
-      </BrowserRouter>
-  );
+	return (
+		<UserContextProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<LoginPage />} />
+					<Route path="/:dashboard" element={<DashboardPage />} />
+					<Route path="/activities" element={<ActivityPage />} />
+					<Route path="/password-reset" element={<PasswordResetPage />} />
+					<Route path="/statistics" element={<StatisticsPage />} />
+				</Routes>
+			</BrowserRouter>
+		</UserContextProvider>
+	);
 }
 
 export default App;
--- frontend/src/route_helper/routes_helper.tsx
+++ frontend/src/route_helper/routes_helper.tsx
@@ -1,25 +1,40 @@
-
 // paths
-const hostPath = '${window.location.origin}';
-const rootPath = '/';
-const dashboard = '/dashboard';
-const ticketHistory = '/ticket_history';
-
+const hostPath = "${window.location.origin}";
+const rootPath = "/";
+const dashboard = "/dashboard";
+const ticketHistory = "/ticket_history";
 
 // endpoints
-const EP_department = '/api/department';
-const EP_ticket = '/api/ticket';
-const EP_login = '/api/login';
+const EP_department = "/api/department";
+const EP_ticket = "/api/ticket";
+const EP_ticket_create = EP_ticket + "/create";
+const EP_login = "/api/login";
 
-const EP_logout = '/api/logout'
+const EP_logout = "/api/logout";
 
 export {
-    hostPath,
-    rootPath,
-    dashboard,
-    EP_department,
-    EP_ticket,
-    ticketHistory,
-    EP_login,
-    EP_logout
+	dashboard,
+	EP_department,
+	EP_login,
+	EP_logout,
+	EP_ticket,
+	EP_ticket_create,
+	hostPath,
+	rootPath,
+	ticketHistory,
 };
+
+// helper functions
+
+export function appendAuthHeader(headers?: HeadersInit) {
+	if (window.sessionStorage.getItem("Authorization")) {
+		return {
+			...headers,
+			Authorization: window.sessionStorage.getItem("Authorization") as string,
+		} as HeadersInit;
+	} else {
+		return {
+			...headers,
+		} as HeadersInit;
+	}
+}
