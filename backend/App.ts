b81c9f73f2a3b128d292c76f6fee2b674a5e92ee
Nini3010 <stefan-wesely@gmx.at>
2025-01-10 21:38:21 +0100

    jwt middleware now sets variable to pass along user_id of communicating user

--- backend/App.ts
+++ backend/App.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
-import { cors } from "hono/cors";
+// import { cors } from "hono/cors";
 import login from "@backend/route/Login.ts";
 import test from "@backend/route/Test.ts";
 import database from "@backend/service/database.ts";
@@ -8,24 +8,24 @@ import logout from "@backend/route/Logout.ts";
 import department from "@backend/route/Department.ts";
 import tickets from "@backend/route/Tickets.ts";
 
-const app: Hono = new Hono().basePath("/api");
+const app = new Hono().basePath("/api");
 database.initDB();
 
 /* Custom logger https://hono.dev/docs/middleware/builtin/logger */
 app.use(logger());
 // set cors policy
 
-app.use(
-  "*",
-  /*cors({
-    origin: (origin, c) => {
-      return origin.includes("localhost") ? origin : "http://localhost:*";
-    },
-    // allowMethods: ['POST', 'GET', 'OPTIONS'],
-    // exposeHeaders: ['Set-Cookie'],
-    credentials: true,
-  }),*/
-);
+// app.use(
+//   "*",
+//   /*cors({
+//     origin: (origin, c) => {
+//       return origin.includes("localhost") ? origin : "http://localhost:*";
+//     },
+//     // allowMethods: ['POST', 'GET', 'OPTIONS'],
+//     // exposeHeaders: ['Set-Cookie'],
+//     credentials: true,
+//   }),*/
+// );
 
 app.route("/login", login);
 app.route("/logout", logout);
--- backend/handler/AuthenticationHandler.ts
+++ backend/handler/AuthenticationHandler.ts
@@ -5,13 +5,18 @@ import { Next } from "hono/types";
 import { createMiddleware } from "hono/factory";
 import { jwt_custom_payload } from "@backend/model/api_types.ts";
 import db from "@backend/service/database.ts";
+import { Action } from "@shared/shared_types.ts";
 
 const JWT_SECRET = Deno.env.get("JWT_SECRET")!;
 const JWT_ACCESS_EXPIRY = parseInt(Deno.env.get("JWT_ACCESS_EXPIRY")!);
 const JWT_REFRESH_SECRET = Deno.env.get("JWT_REFRESH_SECRET")!;
 // const JWT_REFRESH_EXPIRY = convert_to_seconds(Deno.env.get('JWT_REFRESH_EXPIRY')!);
 
-const JWTAuthChecker = createMiddleware(
+const JWTAuthChecker = createMiddleware<{
+  Variables: {
+    user_id: string;
+  };
+}>(
   async (c: Context, next: Next) => {
     const accessToken = c.req.header("Authorization");
     const refreshToken = getCookie(c, "refreshToken");
@@ -36,7 +41,53 @@ const JWTAuthChecker = createMiddleware(
     if (!accessToken && !refreshToken) {
       return c.json({ message: "Unauthorized" }, 401);
     }
-
+    let user_id = undefined;
+    try {
+      user_id = (await verify(
+        accessToken!,
+        JWT_SECRET,
+      ) as jwt_custom_payload).user_id;
+    } catch (_error1) {
+      if (!refreshToken) {
+        return c.json({ message: "Unauthorized" }, 401);
+      }
+      try {
+        user_id =
+          (await verify(refreshToken, JWT_REFRESH_SECRET) as jwt_custom_payload)
+            .user_id;
+      } catch (_error2) {
+        console.log("_error1\n");
+        console.log(_error1);
+        console.log("_error2\n");
+        console.log(_error2);
+        return c.json({ message: "Invalid Token." }, 400);
+      }
+    }
+    const user = db.getUserById(user_id);
+    if ((user == undefined)) {
+      c.header("Authorization", "");
+      deleteCookie(c, "refreshToken");
+      return c.json({ message: "User does not exist" }, 401);
+    }
+    if ((user instanceof Error)) {
+      console.log(user);
+      return c.json({}, 500);
+    }
+    if (!accessToken) {
+      const iat = Math.floor(Date.now() / 1000);
+      const a_exp = JWT_ACCESS_EXPIRY + iat;
+      const accessToken = await sign(
+        {
+          user_id: user_id,
+          iat: iat,
+          exp: a_exp,
+        },
+        JWT_SECRET,
+      );
+      c.header("Authorization", accessToken);
+    }
+    c.set(c.var.user_id, user_id);
+    /*
     try {
       const decoded = await verify(
         accessToken!,
@@ -82,7 +133,21 @@ const JWTAuthChecker = createMiddleware(
         return c.json({ message: "Invalid Token." }, 400);
       }
     }
+      */
     await next();
+  },
+);
+
+const ActionAuth = createMiddleware<{
+  Variables: {
+    allowed_actions: Action[];
+  };
+}>(
+  async (c: Context, next: Next) => {
+    // TODO: proper auth checking
+    // const user = db.getRoleOfUser(user_id);
+    // if(c.var.user_id == )
+    return c.json({ message: "Forbidden" }, 403);
   },
 );
 
