cbd5ea4544e6996e9c6422244517e5ffb2cb97a7
Haris Kurtagic <haris.kurtagic@stud.fh-campuswien.ac.at>
2025-01-13 16:05:22 +0100

    ~ edit path

--- backend/service/dbconnector.ts
+++ backend/service/dbconnector.ts
@@ -5,7 +5,6 @@ import * as path from "jsr:@std/path";
 const sequelize = new Sequelize({
   database: "HelpMeImStuck",
   dialect: "sqlite",
-  storage: (path.dirname(import.meta.url), "test.db"),
+  storage: (path.join(path.dirname(import.meta.url), "test.db").substring(6)),
 });
-
 export { sequelize };
