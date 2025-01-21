ae0e712317dc5f1e4ec3b4086524548062e3efeb
Nini3010 <stefan-wesely@gmx.at>
2025-01-21 23:56:07 +0100

    /api/user now returns a User object

--- backend/route/User.ts
+++ backend/route/User.ts
@@ -2,14 +2,14 @@ import { Hono } from "hono";
 import { validator } from "hono/validator";
 import { AlgorithmName, verify as crypto_verify } from "jsr:@stdext/crypto/hash";
 import {
-	createJWTAuthToken,
-	createJWTRefreshToken,
-	JWTAuthController,
-	removeJWTTokens,
+    createJWTAuthToken,
+    createJWTRefreshToken,
+    JWTAuthController,
+    removeJWTTokens,
 } from "@backend/controller/AuthenticationController.ts";
 import {
-	DepartmentIDValidator,
-	UserIDValidator,
+    DepartmentIDValidator,
+    UserIDValidator,
 } from "@backend/controller/ValidationController.ts";
 import * as db2 from "@backend/service/dbController.ts";
 import { S_User, S_UserAdmin, S_UserCreate, S_UserLogin } from "@shared/shared_schemas.ts";
@@ -18,135 +18,139 @@ import { S_ServersideUser } from "@backend/schemes_and_types/serverside_schemas.
 const user = new Hono();
 
 user.post(
-	"/login",
-	validator("json", (value, c) => {
-		const parsed = S_UserLogin.safeParse(value);
-		if (!parsed.success) {
-			return c.json({ message: "Not a valid Object" }, 400);
-		}
-		return parsed.data;
-	}),
-	async (c) => {
-		const login_user = c.req.valid("json");
-		const user = S_ServersideUser.safeParse(
-			(await db2.getUser({ user_name: login_user.user_name }))?.toJSON(),
-		);
-		if (
-			!user.success ||
-			!crypto_verify(AlgorithmName.Argon2, login_user.password, user.data.password)
-		) {
-			return c.json({ error: "Invalid Credentials" }, 401);
-		}
-		// TODO create token uuid, by saving to DB -> create token only with token uuid
-		await createJWTAuthToken(c, { user_id: user.data.user_id });
-		await createJWTRefreshToken(c, { user_id: user.data.user_id });
-		return c.json({ user_id: user.data.user_id, user_name: user.data.user_name }, 200);
-	},
+    "/login",
+    validator("json", (value, c) => {
+        const parsed = S_UserLogin.safeParse(value);
+        if (!parsed.success) {
+            return c.json({ message: "Not a valid Object" }, 400);
+        }
+        return parsed.data;
+    }),
+    async (c) => {
+        const login_user = c.req.valid("json");
+        const u = await db2.getUser({ user_name: login_user.user_name });
+        if (!u) {
+            return c.json({ error: "Invalid Credentials" }, 401);
+        }
+        const user = S_ServersideUser.safeParse(u.toJSON());
+        if (
+            !user.success ||
+            !crypto_verify(AlgorithmName.Argon2, login_user.password, user.data.password)
+        ) {
+            return c.json({ error: "Invalid Credentials" }, 401);
+        }
+        // TODO create token uuid, by saving to DB -> create token only with token uuid
+        await createJWTAuthToken(c, { user_id: user.data.user_id });
+        await createJWTRefreshToken(c, { user_id: user.data.user_id });
+        return c.json({ user_id: user.data.user_id, user_name: user.data.user_name }, 200);
+    },
 );
 
 user.post("/logout", JWTAuthController, (c) => {
-	// console.log(
-	// 	"logout initiated with access token: \n" + c.req.header("Authorization"),
-	// );
-	removeJWTTokens(c);
-	// TODO delete token from DB
-	return c.json({ message: "User logged out successfully" }, 200);
+    // console.log(
+    // 	"logout initiated with access token: \n" + c.req.header("Authorization"),
+    // );
+    removeJWTTokens(c);
+    // TODO delete token from DB
+    return c.json({ message: "User logged out successfully" }, 200);
 });
 
 // get data of user itself
 user.get("/", JWTAuthController, async (c) => {
-	const user_model = await db2.getUser({ user_id: c.var.user_id });
-	if (!user_model) {
-		return c.json({ error: "Invalid Credentials" }, 401);
-	}
-	const user = S_ServersideUser.safeParse(user_model.toJSON());
-	if (!user.success) {
-		return c.json({ message: "Serverside error" }, 500);
-	}
-	return c.json({ user_id: user.data.user_id, user_name: user.data.user_name }, 200);
+    const user_model = await db2.getUser({ user_id: c.var.user_id });
+    if (!user_model) {
+        return c.json({ error: "Invalid Credentials" }, 401);
+    }
+    const server_user = S_ServersideUser.safeParse(user_model.toJSON());
+    if (!server_user.success) {
+        return c.json({ message: "Serverside error" }, 500);
+    }
+    const user = S_User.parse(server_user);
+
+    return c.json(user, 200);
 });
 
 // create
 user.post(
-	"/",
-	JWTAuthController,
-	// failure potential as it checks for parameter
-	// UserValidator([Actions.user_create], [Actions.user_ownDeartment_create]),
-	validator("json", (value, c) => {
-		const parsed = S_UserCreate.safeParse(value);
-		if (!parsed.success) {
-			return c.json({ message: "Not a valid Object" }, 400);
-		}
-		return parsed.data;
-	}),
-	async (c) => {
-		const user_create_success = await db2.addUser(c.req.valid("json"));
+    "/",
+    JWTAuthController,
+    // failure potential as it checks for parameter
+    // UserValidator([Actions.user_create], [Actions.user_ownDeartment_create]),
+    validator("json", (value, c) => {
+        const parsed = S_UserCreate.safeParse(value);
+        if (!parsed.success) {
+            return c.json({ message: "Not a valid Object" }, 400);
+        }
+        return parsed.data;
+    }),
+    async (c) => {
+        const user_create_success = await db2.addUser(c.req.valid("json"));
 
-		if (!user_create_success) {
-			return c.json({ message: "User creation failed" }, 500);
-		}
-		return c.json({ message: "Successfully created User" }, 200);
-	},
+        if (!user_create_success) {
+            return c.json({ message: "User creation failed" }, 500);
+        }
+        return c.json({ message: "Successfully created User" }, 200);
+    },
 );
 // modify
 user.put(
-	"/:user_id",
-	JWTAuthController,
-	// UserValidator([Actions.user_modify], [Actions.user_ownDeartment_modify]),
-	UserIDValidator(),
-	validator("json", (value, c) => {
-		const parsed = S_UserAdmin.safeParse(value);
-		if (!parsed.success) {
-			return c.json({ message: "Not a valid User Object" }, 400);
-		}
-		return parsed.data;
-	}),
-	async (c) => {
-		if (c.req.valid("param") != c.req.valid("json").user_id) {
-			return c.json({ message: "User ID of path and body does not match!" }, 400);
-		}
-		// deny update if last role of user would be deleted
-		if (!c.req.valid("json").roles) {
-			return c.json({ message: "User must have at least one Role!" }, 400);
-		}
-		const updated_user_model = await db2.editUser(c.req.valid("json"));
-		if (!updated_user_model) {
-			return c.json({ message: "User modification failed" }, 500);
-		}
-		const updated_user = S_User.safeParse(updated_user_model.toJSON());
-		if (!updated_user.success) {
-			console.error(updated_user.error);
-			return c.json({ message: "Serverside error" }, 500);
-		}
+    "/:user_id",
+    JWTAuthController,
+    // UserValidator([Actions.user_modify], [Actions.user_ownDeartment_modify]),
+    UserIDValidator(),
+    validator("json", (value, c) => {
+        const parsed = S_UserAdmin.safeParse(value);
+        if (!parsed.success) {
+            return c.json({ message: "Not a valid User Object" }, 400);
+        }
+        return parsed.data;
+    }),
+    async (c) => {
+        if (c.req.valid("param") != c.req.valid("json").user_id) {
+            return c.json({ message: "User ID of path and body does not match!" }, 400);
+        }
+        // deny update if last role of user would be deleted
+        if (!c.req.valid("json").roles) {
+            return c.json({ message: "User must have at least one Role!" }, 400);
+        }
+        const updated_user_model = await db2.editUser(c.req.valid("json"));
+        if (!updated_user_model) {
+            return c.json({ message: "User modification failed" }, 500);
+        }
+        const updated_user = S_User.safeParse(updated_user_model.toJSON());
+        if (!updated_user.success) {
+            console.error(updated_user.error);
+            return c.json({ message: "Serverside error" }, 500);
+        }
 
-		return c.json(updated_user.data, 200);
-	},
+        return c.json(updated_user.data, 200);
+    },
 );
 // delete
 user.delete(
-	"/:user_id",
-	JWTAuthController,
-	// UserValidator([Actions.user_delete], [Actions.user_ownDeartment_delete]),
-	UserIDValidator(),
-	async (c) => {
-		const user_id = c.req.valid("param");
-		const user_delete_success = await db2.deleteUser(user_id);
-		if (!user_delete_success) {
-			return c.json({ message: "User deletion failed" }, 500);
-		}
-		return c.json({ message: "Successfully deleted User" }, 200);
-	},
+    "/:user_id",
+    JWTAuthController,
+    // UserValidator([Actions.user_delete], [Actions.user_ownDeartment_delete]),
+    UserIDValidator(),
+    async (c) => {
+        const user_id = c.req.valid("param");
+        const user_delete_success = await db2.deleteUser(user_id);
+        if (!user_delete_success) {
+            return c.json({ message: "User deletion failed" }, 500);
+        }
+        return c.json({ message: "Successfully deleted User" }, 200);
+    },
 );
 
 // get all users of provided department
 user.get(
-	"/dept/:department_id",
-	JWTAuthController,
-	DepartmentIDValidator(),
-	async (c) => {
-		const users = await db2.getAllUsersInDepartment(c.req.valid("param"));
-		return c.json(users, 200);
-	},
+    "/dept/:department_id",
+    JWTAuthController,
+    DepartmentIDValidator(),
+    async (c) => {
+        const users = await db2.getAllUsersInDepartment(c.req.valid("param"));
+        return c.json(users, 200);
+    },
 );
 
 export default user;
--- backend/service/dbSetup.ts
+++ backend/service/dbSetup.ts
@@ -12,122 +12,122 @@ import Image from "@backend/model/Image.ts";
 import Role from "@backend/model/Role.ts";
 
 export default async (): Promise<void> => {
-	new Action();
-	new Department();
-	new Event();
-	new EventType();
-	new Image();
-	new Role();
-	new Status();
-	new Tag();
-	new Ticket();
-	new User();
+    new Action();
+    new Department();
+    new Event();
+    new EventType();
+    new Image();
+    new Role();
+    new Status();
+    new Tag();
+    new Ticket();
+    new User();
 
-	//1
-	User.belongsToMany(Action, {
-		through: "UserAction",
-		foreignKey: "fk_user_id",
-		as: "actions",
-	});
-	Action.belongsToMany(User, {
-		through: "UserAction",
-		foreignKey: "fk_action_id",
-	});
+    //1
+    User.belongsToMany(Action, {
+        through: "UserAction",
+        foreignKey: "fk_user_id",
+        as: "actions",
+    });
+    Action.belongsToMany(User, {
+        through: "UserAction",
+        foreignKey: "fk_action_id",
+    });
 
-	//2
-	Action.belongsToMany(Role, {
-		through: "RoleAction",
-		foreignKey: "fk_action_id",
-	});
-	Role.belongsToMany(Action, {
-		through: "RoleAction",
-		foreignKey: "fk_role_id",
-		as: "actions",
-	});
+    //2
+    Action.belongsToMany(Role, {
+        through: "RoleAction",
+        foreignKey: "fk_action_id",
+    });
+    Role.belongsToMany(Action, {
+        through: "RoleAction",
+        foreignKey: "fk_role_id",
+        as: "actions",
+    });
 
-	//User and Role association
-	User.belongsToMany(Role, {
-		through: "UserRoles",
-		foreignKey: "fk_user_id",
-		as: "roles",
-	});
-	Role.belongsToMany(User, {
-		through: "UserRoles",
-		foreignKey: "fk_role_id",
-		onDelete: "RESTRICT",
-	});
+    //User and Role association
+    User.belongsToMany(Role, {
+        through: "UserRoles",
+        foreignKey: "fk_user_id",
+        as: "roles",
+    });
+    Role.belongsToMany(User, {
+        through: "UserRoles",
+        foreignKey: "fk_role_id",
+        onDelete: "RESTRICT",
+    });
 
-	//Roles
-	//One Department has many Roles
-	Department.hasMany(Role, { foreignKey: "fk_department_id", onDelete: "CASCADE" });
-	Role.belongsTo(Department, {
-		foreignKey: "fk_department_id",
-		as: "department",
-	});
+    //Roles
+    //One Department has many Roles
+    Department.hasMany(Role, { foreignKey: "fk_department_id", onDelete: "CASCADE" });
+    Role.belongsTo(Department, {
+        foreignKey: "fk_department_id",
+        as: "department",
+    });
 
-	//4
-	Ticket.belongsToMany(Tag, {
-		through: "TicketTag",
-		foreignKey: "fk_ticket_id",
-	});
-	Tag.belongsToMany(Ticket, { through: "TicketTag", foreignKey: "fk_tag_id" });
+    //4
+    Ticket.belongsToMany(Tag, {
+        through: "TicketTag",
+        foreignKey: "fk_ticket_id",
+    });
+    Tag.belongsToMany(Ticket, { through: "TicketTag", foreignKey: "fk_tag_id" });
 
-	//5
-	Ticket.belongsToMany(Department, {
-		through: "DepartmentTicket",
-		foreignKey: "fk_ticket_id",
-		as: "departments",
-	});
-	Department.belongsToMany(Ticket, {
-		through: "DepartmentTicket",
-		foreignKey: "fk_department_id",
-		onDelete: "NO ACTION",
-	});
+    //5
+    Ticket.belongsToMany(Department, {
+        through: "DepartmentTicket",
+        foreignKey: "fk_ticket_id",
+        as: "departments",
+    });
+    Department.belongsToMany(Ticket, {
+        through: "DepartmentTicket",
+        foreignKey: "fk_department_id",
+        onDelete: "NO ACTION",
+    });
 
-	//Events
-	//Multiple Events can have the same EventType
-	EventType.hasMany(Event, { foreignKey: "fk_event_type_id" });
-	Event.belongsTo(EventType, { foreignKey: "fk_event_type_id" });
-	//One User can have multiple Events
-	User.hasMany(Event, { foreignKey: "fk_user_id" });
-	Event.belongsTo(User, { foreignKey: "fk_user_id", as: "author" });
-	//One Ticket can have many Events
-	Ticket.hasMany(Event, { foreignKey: "fk_ticket_id", onDelete: "CASCADE" });
-	Event.belongsTo(Ticket, { foreignKey: "fk_ticket_id" });
-	//One Image belongs to one Event / One Event can have multiple Images
-	Image.belongsTo(Event, { foreignKey: "fk_event_id" });
-	Event.hasMany(Image, { foreignKey: "fk_event_id", onDelete: "CASCADE" });
+    //Events
+    //Multiple Events can have the same EventType
+    EventType.hasMany(Event, { foreignKey: "fk_event_type_id" });
+    Event.belongsTo(EventType, { foreignKey: "fk_event_type_id" });
+    //One User can have multiple Events
+    User.hasMany(Event, { foreignKey: "fk_user_id" });
+    Event.belongsTo(User, { foreignKey: "fk_user_id", as: "author" });
+    //One Ticket can have many Events
+    Ticket.hasMany(Event, { foreignKey: "fk_ticket_id", onDelete: "CASCADE" });
+    Event.belongsTo(Ticket, { foreignKey: "fk_ticket_id" });
+    //One Image belongs to one Event / One Event can have multiple Images
+    Image.belongsTo(Event, { foreignKey: "fk_event_id" });
+    Event.hasMany(Image, { foreignKey: "fk_event_id", onDelete: "CASCADE" });
 
-	//Tags
-	//One Department has many Tags
-	Department.hasMany(Tag, { foreignKey: "fk_department_id" });
-	Tag.belongsTo(Department, { foreignKey: "fk_department_id" });
+    //Tags
+    //One Department has many Tags
+    Department.hasMany(Tag, { foreignKey: "fk_department_id", onDelete: "CASCADE" });
+    Tag.belongsTo(Department, { foreignKey: "fk_department_id" });
 
-	//Tickets
-	//One User can have multiple Tickets
-	User.hasMany(Ticket, { foreignKey: "fk_user_id", onDelete: "NO ACTION" });
-	Ticket.belongsTo(User, { foreignKey: "fk_user_id", as: "author" });
-	// Multiple Tickets can have the same status
-	Status.hasMany(Ticket, { foreignKey: "fk_status_id" });
-	Ticket.belongsTo(Status, { foreignKey: "fk_status_id" });
-	// One Tickets can have multiple images / One Image blengs to one Event
-	Image.belongsTo(Ticket, { foreignKey: "fk_ticket_id" });
-	Ticket.hasMany(Image, { foreignKey: "fk_ticket_id", onDelete: "CASCADE" });
+    //Tickets
+    //One User can have multiple Tickets
+    User.hasMany(Ticket, { foreignKey: "fk_user_id", onDelete: "NO ACTION" });
+    Ticket.belongsTo(User, { foreignKey: "fk_user_id", as: "author" });
+    // Multiple Tickets can have the same status
+    Status.hasMany(Ticket, { foreignKey: "fk_status_id" });
+    Ticket.belongsTo(Status, { foreignKey: "fk_status_id" });
+    // One Tickets can have multiple images / One Image blengs to one Event
+    Image.belongsTo(Ticket, { foreignKey: "fk_ticket_id" });
+    Ticket.hasMany(Image, { foreignKey: "fk_ticket_id", onDelete: "CASCADE" });
 
-	try {
-		await sequelize.sync({ force: true });
-		const queryInterface = sequelize.getQueryInterface();
-		await queryInterface.addConstraint("Tags", {
-			fields: ["fk_department_id", "tag_name"],
-			type: "unique",
-			name: "UniqueTagInDepartment",
-		});
-		await queryInterface.addConstraint("Tags", {
-			fields: ["fk_department_id", "tag_abbreviation"],
-			type: "unique",
-			name: "UniqueTagAbbreviationInDepartment",
-		});
-	} catch (error) {
-		throw error;
-	}
+    try {
+        await sequelize.sync({ force: true });
+        const queryInterface = sequelize.getQueryInterface();
+        await queryInterface.addConstraint("Tags", {
+            fields: ["fk_department_id", "tag_name"],
+            type: "unique",
+            name: "UniqueTagInDepartment",
+        });
+        await queryInterface.addConstraint("Tags", {
+            fields: ["fk_department_id", "tag_abbreviation"],
+            type: "unique",
+            name: "UniqueTagAbbreviationInDepartment",
+        });
+    } catch (error) {
+        throw error;
+    }
 };
